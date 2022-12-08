6918f95f41ce8cc87ed267f92e0f60f59f26549f
ImproveEu <100429135@alumnos.uc3m.es>
2022-12-08 17:35:09 +0100

    fixedjson

--- jsonfixer.py
+++ jsonfixer.py
@@ -1,12 +1,45 @@
 import json
 import regex as re
 
-f = open("movies2.json", "r")
+def intersperse(lst, item):
+    result = [item] * (len(lst) * 2 - 1)
+    result[0::2] = lst
+    return result
+
+with open ('movies.json', "r") as f:
+    data = json.load(f)
+
+print(type(data))
+fixed_list = intersperse(data,{"index":{}})
+
+j = json.dumps(fixed_list, indent=0)
+with open('letssee.json', 'w') as f:
+    f.write(j)
+    f.close
+
+f = open("letssee.json", "r")
 a = f.read()
 
 a = a.replace("},", "}")
 with open('movies.txt', 'w') as f:
     f.write(a)
+
+
+
+
+""""
+b = json.loads(a)
+j = json.dumps(b, indent=4)
+with open('fixex_bulk_elastic.json', 'w') as f:
+    f.write(j)
+    f.close
+
+j = json.dumps(fixed_list, indent=0)
+with open('letssee.json', 'w') as f:
+    f.write(j)
+    f.close"""
+
+
 #b = json.loads(a)
 #j = json.dumps(b, indent=4)
 #with open('fixex_bulk_elastic.json', 'w') as f:
